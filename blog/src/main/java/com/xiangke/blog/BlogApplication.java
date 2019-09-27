f2f6ed80a07d81541bfb9fb29470a93e7053a02b
xiangke8967 <a6574823@qq.com>
2019-09-28 00:52:58 +0800

    update

--- blog/src/main/java/com/xiangke/blog/BlogApplication.java
+++ blog/src/main/java/com/xiangke/blog/BlogApplication.java
@@ -1,4 +1,4 @@
-package com.xk.blog;
+package com.xiangke.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- blog/src/test/java/com/xiangke/blog/BlogApplicationTests.java
+++ blog/src/test/java/com/xiangke/blog/BlogApplicationTests.java
@@ -1,4 +1,4 @@
-package com.xk.blog;
+package com.xiangke.blog;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
